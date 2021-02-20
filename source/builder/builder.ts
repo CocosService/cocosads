ebe520c42386557e884a0f1af738b0bb9939b452
xin.yang <953459485@qq.com>
2021-02-20 14:39:56 +0800

    完善 builder 接口

--- source/builder/builder.ts
+++ source/builder/builder.ts
@@ -1,23 +1,30 @@
+import { ParamType } from "../../@types/service/ifs";
+
+export async function onSettings(params: ParamType, previewType: "browser" | "simulator", insertJS: (js: string) => void) {
+    console.log(__dirname, 'cocosads => onSettings');
+    // 通过调用 insertJS 向 settings.jsList 中 插入项目
+}
+
 export async function onServiceStatusChanged(projectPath: string, info: any) {
-    console.log('analytics => onServiceStatusChanged');
-    console.log(projectPath, info);
-    // @ts-ignore
-    console.log(ccService);
+    console.log(__dirname, 'cocosads => onServiceStatusChanged');
+    console.log(__dirname, projectPath, info);
+    console.log(__dirname, ccService);
 }
 
 export async function onBeforeBuild(options: any, result: any) {
-    console.log('analytics => onBeforeBuild');
+    console.log(__dirname, 'cocosads => onBeforeBuild');
+    // 抛出异常则终止构建流程
     // throw new Error("我是故意的，就是不想让你集成成功！");
 }
 
 export async function onBeforeCompressSettings(options: any, result: any) {
-    console.log('analytics => onBeforeCompressSettings');
+    console.log(__dirname, 'cocosads => onBeforeCompressSettings');
 }
 
 export async function onAfterCompressSettings(options: any, result: any) {
-    console.log('analytics => onAfterCompressSettings');
+    console.log(__dirname, 'cocosads => onAfterCompressSettings');
 }
 
 export async function onAfterBuild(options: any, result: any) {
-    console.log('analytics => onAfterBuild');
+    console.log(__dirname, 'cocosads => onAfterBuild');
 }
